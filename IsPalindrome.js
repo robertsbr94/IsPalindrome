dcd4c28f0e17f708cfe570dcb0ef8d49e0901d8f
robertsbr94 <63520576+robertsbr94@users.noreply.github.com>
2020-05-09 15:35:53 +0100

    Add files via upload
    Initial Commit

---
+++ IsPalindrome.js
@@ -0,0 +1,11 @@
+function PalindromeResult(inStr) {
+    let result = true;
+    for (let i = 0; i < inStr.length; i++) {
+        if (inStr[i] !== inStr[inStr.length - i - 1]) {
+            result = false;
+        }
+    }
+    return result
+}
+
+export const IsPalindrome = (str, ignoreCapitalisation = false) => ignoreCapitalisation == true ? PalindromeResult(str.toUpperCase()) : PalindromeResult(str);
